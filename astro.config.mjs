641eac26831504d12c97e658c440726032fdb186
Angelos Chalaris <chalarangelo@gmail.com>
2024-12-30 15:33:54 +0200

    Update dependencies and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    inlineStylesheets: 'always',
+  },
+  compressHTML: true,
+});
